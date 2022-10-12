165439131c901bfb05aff1bf49939e5fb0648c51
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-10-12 01:07:04 -0300

    feat: adiciona events

--- EVENTO-WEB/routes/web.php
+++ EVENTO-WEB/routes/web.php
@@ -1,18 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Notificacao\NotificacaoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/contact', function () {
+    return view('contact');
+    Route::get('/teste1', function () {
+        return view('teste1');
+    });
+});
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::controller(NotificacaoController::class)->group(function(){
+    Route::get('/notificacao', 'viewDeNotification')->name('notificacao.viewDeNotification');
+    Route::get('/notificacao/detalhes', 'viewDeDetalhes')->name('notificacao.viewDeDetalhes');
 });
