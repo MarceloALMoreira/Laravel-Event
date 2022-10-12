3df4a0fd21ffd5f375c295649baeee322747febb
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-10-12 02:35:08 -0300

    feat: user dono do event

--- EVENTO-WEB/routes/web.php
+++ EVENTO-WEB/routes/web.php
@@ -5,24 +5,16 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\Notificacao\NotificacaoController;
 
 Route::get('/', [EventController::class, 'index']);
-Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function () {
-    return view('contact');
-    Route::get('/teste1', function () {
-        return view('teste1');
-    });
+    return view('contact');    
 });
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 });
 
 Route::controller(NotificacaoController::class)->group(function(){
