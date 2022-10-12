d48ad4e95854bbfad465897c985aeb27cd458db5
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-10-12 19:18:00 -0300

    feat: busca user banco para view dono do evento

--- EVENTO-WEB/resources/views/layouts/main.blade.php
+++ EVENTO-WEB/resources/views/layouts/main.blade.php
@@ -77,7 +77,7 @@
     </div>
   </main>
   <footer>
-    <p>HDC Events &copy; 2020</p>
+    <p>HDC Events &copy; 2022</p>
   </footer>
   <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 </body>
