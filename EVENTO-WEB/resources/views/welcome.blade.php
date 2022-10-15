7b686340e3fc71bd7a3ccd86cc74f7d174586e4e
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-10-14 23:22:15 -0300

    feat: relação many to many event_user

--- EVENTO-WEB/resources/views/welcome.blade.php
+++ EVENTO-WEB/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'EVENTO WEB')
+@section('title', 'HDC Events')
 
 @section('content')
 
@@ -12,27 +12,27 @@
 </div>
 <div id="events-container" class="col-md-12">
     @if($search)
-    <h2>Buscando por: {{$search}}</h2>
+    <h2>Buscando por: {{ $search }}</h2>
     @else
     <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
-    @endif    
+    @endif
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
             <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
-                <p class="card-date">{{date('d/m/Y', strtotime($event->date))}}}</p>
+                <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
-                <p class="card-participants">X Participantes</p>
+                <p class="card-participants"> {{ count($event->users) }} Participantes</p>
                 <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         @endforeach
-        @if(count($events)  == 0 && $search)
-        <p>Não foi possivel encontrar nemhum enveto com : {{$search}}! <a href="/">Ver todos</a></p>
-        @elseif(count($events) ==0)
-        <p>Não há eventos disponíveis</p>
+        @if(count($events) == 0 && $search)
+            <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos</a></p>
+        @elseif(count($events) == 0)
+            <p>Não há eventos disponíveis</p>
         @endif
     </div>
 </div>
